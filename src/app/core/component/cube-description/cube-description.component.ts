a0216f9dad7459e53f4269e22a930f2c472b1758
Hofstetter Benjamin (extern) <benjamin.hofstetter@mobi.ch>
2024-05-24 14:51:00 +0200

    fix language in cube view

--- src/app/core/component/cube-description/cube-description.component.ts
+++ src/app/core/component/cube-description/cube-description.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, Signal, inject, input, output, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, Signal, computed, inject, input, output, signal } from '@angular/core';
 import { CubeDefinition } from '../../model/cube-definition/cube-definition';
 
 import { MatIconModule } from '@angular/material/icon';
@@ -24,6 +24,45 @@ export class CubeDescriptionComponent {
   cube = input.required<CubeDefinition | undefined>();
   selectedProfileIri = input.required<string | undefined | null>();
   profileSelected = output<ValidationProfile>();
+
+  readonly #languageService = inject(ObLanguageService);
+  readonly #destroyRef = inject(DestroyRef);
+  readonly #language: Signal<string | undefined>;
+
+  constructor() {
+    this.#language = toSignal<string>(this.#languageService.locale$.pipe(
+      takeUntilDestroyed(this.#destroyRef),
+      map(locale => locale.split('-')[0] ?? 'de')
+    )
+    );
+  }
+
+  cubeName = computed<string>(() => {
+    const lang = this.#language() ?? 'de';
+    const cube = this.cube();
+    if (!cube) {
+      return '';
+    }
+    const name = cube.name;
+    const nameDE = cube.nameDE;
+    const nameEN = cube.nameEN;
+    const nameFR = cube.nameFR;
+    const nameIT = cube.nameIT;
+
+    switch (lang) {
+      case 'de':
+        return nameDE ?? name ?? nameEN ?? nameFR ?? nameIT ?? 'kein Name';
+      case 'en':
+        return nameEN ?? name ?? nameDE ?? nameFR ?? nameIT ?? 'no name';
+      case 'fr':
+        return nameFR ?? name ?? nameDE ?? nameEN ?? nameIT ?? 'pas de nom';
+      case 'it':
+        return nameIT ?? name ?? nameDE ?? nameEN ?? nameFR ?? 'nessun nome';
+      default:
+        return nameDE ?? name ?? nameEN ?? nameFR ?? nameIT ?? 'kein Name';
+    }
+
+  });
 
   emitProfileSelected(profile: ValidationProfile) {
     this.profileSelected.emit(profile);
